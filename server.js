adcee3261dfc884aee35f5e00a4fc7b36ddafd63
12-kelvin <ndaulakelvin@gmail.com>
2021-12-07 22:03:24 +0800

    initial Books, create index new

--- server.js
+++ server.js
@@ -5,8 +5,10 @@ const app = express()
 const expressLayouts = require ('express-ejs-layouts')
 const bodyParser = require('body-parser')
 
+
 const indexRouter = require('./routes/index')
 const authorRouter = require('./routes/authors')
+const bookRouter = require('./routes/books')
 
 
 app.set('view engine', 'ejs')
@@ -25,5 +27,7 @@ db.once('open', () => console.log('welcome to mongoose kelvin'))
 
 app.use('/',indexRouter)
 app.use('/authors',authorRouter)
+app.use('/books',bookRouter)
+
 
 app.listen(process.env.PORT || 3000)
